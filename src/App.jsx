25c67e8eed04de43668d44bdf50d68884697194c
Kirill1one <kirill2002ku@gmail.com>
2024-07-31 15:14:36 +0300

    Hero section intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Navbar from "./components/Navbar"
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
 
 const App = () => {
 	return (
-	<>
-    <Navbar />
-  </>
+		<>
+			<Navbar />
+			<div className="max-w-7xl mx-auto pt-20 px-6">
+			<HeroSection />
+
+			</div>
+		</>
 	)
 }
 
